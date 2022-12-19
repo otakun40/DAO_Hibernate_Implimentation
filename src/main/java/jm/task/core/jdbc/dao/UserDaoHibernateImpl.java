961a400dddaa4c2e706befe42f2c00361a360292
Bulat Minnigayazov <otakun40@gmail.com>
2022-12-19 10:14:47 +0300

    add rollbacks, add try with resources to autocloseable sessions

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -15,68 +15,70 @@ public class UserDaoHibernateImpl implements UserDao {
     }
 
     @Override
+    @Transactional(rollbackOn = org.hibernate.HibernateException.class)
     public void createUsersTable() {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.createSQLQuery("CREATE TABLE IF NOT EXISTS users(" +
-                "id INT PRIMARY KEY AUTO_INCREMENT," +
-                "name VARCHAR(255)," +
-                "lastName VARCHAR(255)," +
-                "age INT)").executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession()){
+            session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users(" +
+                    "id INT PRIMARY KEY AUTO_INCREMENT," +
+                    "name VARCHAR(255)," +
+                    "lastName VARCHAR(255)," +
+                    "age INT)").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 
     @Override
+    @Transactional(rollbackOn = org.hibernate.HibernateException.class)
     public void dropUsersTable() {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 
 
     @Override
     @Transactional(rollbackOn = org.hibernate.HibernateException.class)
     public void saveUser(String name, String lastName, byte age) {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.persist(new User(name, lastName, age));
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.persist(new User(name, lastName, age));
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     @Transactional(rollbackOn = org.hibernate.HibernateException.class)
     public void removeUserById(long id) {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        User user = session.get(User.class, id);
-        session.remove(user);
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.remove(user);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     @Transactional(rollbackOn = org.hibernate.HibernateException.class)
     public List<User> getAllUsers() {
         List<User> result;
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        result = session.createQuery("SELECT u FROM User u", User.class).getResultList();
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            result = session.createQuery("SELECT u FROM User u", User.class).getResultList();
+            session.getTransaction().commit();
+        }
         return result;
     }
 
     @Override
     @Transactional(rollbackOn = org.hibernate.HibernateException.class)
     public void cleanUsersTable() {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.createQuery("DELETE FROM User u").executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.createQuery("DELETE FROM User u").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 }
