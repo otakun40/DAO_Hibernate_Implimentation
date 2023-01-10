5714104e60ff959a867650efe4bbdf8fc3fc9205
Bulat Minnigayazov <otakun40@gmail.com>
2023-01-10 12:41:32 +0300

    add rollbacks for DML operations

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,10 +2,10 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
-import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -36,47 +36,55 @@ public class UserDaoHibernateImpl implements UserDao {
         }
     }
 
-
     @Override
-    @Transactional(rollbackOn = org.hibernate.HibernateException.class)
     public void saveUser(String name, String lastName, byte age) {
         try (Session session = sessionFactory.getCurrentSession()) {
             session.beginTransaction();
             session.persist(new User(name, lastName, age));
             session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            sessionFactory.getCurrentSession().getTransaction().rollback();
         }
     }
 
     @Override
-    @Transactional(rollbackOn = org.hibernate.HibernateException.class)
     public void removeUserById(long id) {
         try (Session session = sessionFactory.getCurrentSession()) {
             session.beginTransaction();
             User user = session.get(User.class, id);
             session.remove(user);
             session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            sessionFactory.getCurrentSession().getTransaction().rollback();
         }
     }
 
     @Override
-    @Transactional(rollbackOn = org.hibernate.HibernateException.class)
     public List<User> getAllUsers() {
-        List<User> result;
+        List<User> result = new ArrayList<>();
         try (Session session = sessionFactory.getCurrentSession()) {
             session.beginTransaction();
             result = session.createQuery("SELECT u FROM User u", User.class).getResultList();
             session.getTransaction().commit();
+            return result;
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            sessionFactory.getCurrentSession().getTransaction().rollback();
         }
         return result;
     }
 
     @Override
-    @Transactional(rollbackOn = org.hibernate.HibernateException.class)
     public void cleanUsersTable() {
         try (Session session = sessionFactory.getCurrentSession()) {
             session.beginTransaction();
             session.createQuery("DELETE FROM User u").executeUpdate();
             session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            sessionFactory.getCurrentSession().getTransaction().rollback();
         }
     }
 }
